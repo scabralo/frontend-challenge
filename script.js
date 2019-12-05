33c074fe46c8b13e52709d266572fc3cd2af4c3a
Sergio Cabral <sergioantonio60@gmail.com>
2019-12-05 00:43:08 -0500

    Adds multiple constants and variables

--- script.js
+++ script.js
@@ -7,4 +7,17 @@ $(function() {
     message: '',
     weatherIcon: ''
   }
+  let marker;
+  let map;
+  let geocoder;
+
+  const $mapContainer = $('#map-container')
+  const $locationInput = $('#location-input')
+  const $locationForm = $('#location-search')
+
+  // Overlay Elements
+  const $stationName = $('#station-name')
+  const $bikesAvailable = $('#bikes-available')
+  const $docksAvailable = $('#docks-available')
+  const $stationAddress = $('#station-address')
 })
